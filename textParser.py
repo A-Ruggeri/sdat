780e008836cdecc937e523c712669d846476578f
Andrew <andrew@Andrews-MacBook-Pro-2.local>
2024-11-03 10:32:29 -0500

    moved text file parsing to its own file

---
+++ textParser.py
@@ -0,0 +1,37 @@
+import decisionTree
+
+
+def textParser(filePath):
+    cmdFile = open(filePath, 'r')  # todo: test for file existance
+    lines = cmdFile.readlines()
+
+    # Create decisionTree
+    dt = decisionTree.DecisionTree()
+
+    databaseLoad = dict()
+
+    for line in lines:
+        cmds = line.replace('\n', '').split(' ')
+
+        # it's Binary
+        # ----------------------------------------------------------
+        if (len(cmds) == 3) or (len(cmds) == 4):  # hack allow 4 and just drop the 'CAT'
+            dt.addIndependantVar(cmds[0], cmds[1], cmds[2])
+            # fieldpar = (cmds[1], cmds[2])
+            # collectionFieldList = [fieldpar]
+            #
+            # if cmds[0] in databaseLoad:
+            #     databaseLoad[cmds[0]] += collectionFieldList
+            # else:
+            #     databaseLoad[cmds[0]] = collectionFieldList
+
+
+
+        # Else, ignore it
+        # ----------------------------------------------------------
+        else:
+            print("reject current line")
+
+    # All done, it's more fun to compute
+    dt.calculate()
+    return True
