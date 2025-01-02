a6a9d60d25d9eeaf268abebfe63346ec6b9c73bf
Andrew <andrew.ruggeri@technocopia.org>
2025-01-01 22:02:59 -0500

    More updates to NN classes

--- compute/neuralNetwork.py
+++ compute/neuralNetwork.py
@@ -1,62 +1,77 @@
 # imports
 import torch
+import compute.computeBase
 from compute.loging import printInfo
 
+
 class neuralNetworkModel(torch.nn.Module):
+    """
+    Class for neural network model
+    """
     def __init__(self, **kwargs):
-        super(NeuralNetwork, self).__init__()
+        super(neuralNetworkModel, self).__init__()
 
         # default and overwrite
-        input_dim = 1
-        hidden_dim = 1
-        output_dim = 1
+        inputDim = 4
+        hiddenDim = 3
+        outputDim = 1 # should stay at 1
 
         # Read from dict
         self.__dict__.update(kwargs)
 
+        # Layer Sequence
+        # self.torchSeq = torch.nn.Sequential()
+
         # Layer 1
-        self.layer_1 = torch.nn.Linear(input_dim, hidden_dim)
+        self.layer_1 = torch.nn.Linear(inputDim, hiddenDim)
         torch.nn.init.kaiming_uniform_(self.layer_1.weight, nonlinearity='relu')
+        # self.torchSeq.add(self.layer_1)
 
         # Layer 2
-        self.layer_2 = torch.nn.Linear(hidden_dim, output_dim)
+        self.layer_2 = torch.nn.Linear(hiddenDim, outputDim)
+        # self.torchSeq.add(self.layer_2)
 
     def forward(self, x):
         x = torch.nn.functional.relu(self.layer_1(x))
         x = torch.nn.functional.relu(self.layer_2(x))
+        return x
 
 
-class NeuralNetwork:
+class NeuralNetwork(compute.computeBase.computeBase):
     def __init__(self, **kwargs):
-        print("Initializing NeuralNetwork...")
+        super(NeuralNetwork, self).__init__()
+        printInfo("Initializing NeuralNetwork")
 
         self.model = neuralNetworkModel()
         print(self.model)
 
+        # Config parameters
         self.learningRate = 0.1
         self.numEpochs = 1000
 
-        # Read from dict
+        # Read from dict and update
         self.__dict__.update(kwargs)
 
         self.torchDevice = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        printInfo(f"Using: {self.torchDevice}")
+        # self.model.to(self.torchDevice)
+        print(f"Using: {self.torchDevice}")
 
 
-    def calculate(self):
-        printInfo("Calculating NeuralNetwork:")
+    def calculate(self, outputFolder):
+        printInfo("Calculating NeuralNetwork")
 
-        torchX = torch.tensor(self.X_train, dtype=torch.float32)
-        torchY = torch.tensor(self.Y_train, dtype=torch.int8).reshape(-1,1)
+        torchX = torch.tensor(self.dataSource.X_test.values, dtype=torch.float32)
+        torchY = torch.tensor(self.dataSource.y_test.values, dtype=torch.int8)
 
+        lossFunc = torch.nn.BCELoss()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
 
         for epoch in range(self.numEpochs):
             for torchX, torchY in zip(torchX, torchY):
                 optimizer.zero_grad()
+
                 pred = self.model(torchX)
-                loss = torch.nn.BCELoss(pred, torchY.unsqueeze(-1))
-                loss_values.append(loss.item())
+                loss = lossFunc(pred, torchY)
                 loss.backward()
                 optimizer.step()
 
